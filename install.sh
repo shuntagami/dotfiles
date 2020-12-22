ad9dc2cde4aa40e765d98ab818f7e89340cdeb5a
shuntagami <shuntagami23@gmail.com>
2020-12-22 20:58:19 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# add submodule
+git submodule update --init --recursive
+
+# prezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+ ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# symlink dotfiles
+ln -sf ~/dotfiles/.zprezto ~/.zprezto
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/.vim ~/.vim
+
+# change shell
+chsh -s $(which zsh)
+
+source ~/dotfiles/.zshrc
+source ~/dotfiles/.zpreztorc
