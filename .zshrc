90d4eab42e5e501b1c6aa1833639f7f8e8b7dbb0
shuntagami <shuntagami23@gmail.com>
2020-12-29 05:41:26 +0900

    created homebrew_install.sh

--- .zshrc
+++ .zshrc
@@ -12,9 +12,9 @@ fi
 
 # Customize to your needs...
 # path
-export PATH=$HOME/bin:usr/bin:/usr/local/bin:$PATH
+# export PATH=$HOME/bin:usr/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:usr/local/bin:/usr/bin:$PATH
 # anyenv
-eval "$(anyenv init -)"
 # mysql
 export PATH="/usr/local/opt/mysql@5.6/bin:$PATH"
 # npm-global
@@ -23,3 +23,39 @@ export PATH=$HOME/.npm-global/bin:$PATH
 
 # デフォルトエディタの設定
 export EDITOR="vim"
+
+# 直前と同じコマンドラインはヒストリに追加しない
+setopt hist_ignore_dups
+
+# ヒストリにhistoryコマンドを記録しない
+setopt hist_no_store
+
+# 余分なスペースを削除してヒストリに記録する
+setopt hist_reduce_blanks
+
+# 重複したヒストリは追加しない
+setopt hist_ignore_all_dups
+
+# 補完候補が複数ある時に、一覧表示
+setopt auto_list
+
+# 保管結果をできるだけ詰める
+setopt list_packed
+
+# 補完キー（Tab, Ctrl+I) を連打するだけで順に補完候補を自動で補完
+setopt auto_menu
+
+# カッコの対応などを自動的に補完
+setopt auto_param_keys
+
+# ディレクトリ名の補完で末尾の / を自動的に付加し、次の補完に備える
+setopt auto_param_slash
+
+# Ctrl+wで､直前の/までを削除する｡
+WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
+
+# cd をしたときにlsを実行する
+function chpwd() { ls }
+
+# ディレクトリ名だけで､ディレクトリの移動をする｡
+setopt auto_cd
---
+++ homebrew_install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+echo "installing homebrew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "run brew doctor..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "ok. run brew upgrade..."
+
+brew upgrade --all
+
+formulas=(
+    git
+    anyenv
+    openssl
+    docker
+    docker-compose
+    z
+    colordiff
+    "--without-etcdir zsh"
+    zsh-completions
+    "--with-cocoa --srgb emacs"
+    cask
+    hub
+    tig
+    node
+    python3
+    lua
+    "vim --with-lua"
+    mysql
+    composer
+    markdown
+    mecab
+)
+
+"brew tap..."
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap homebrew/homebrew-php
+brew tap homebrew/apache
+brew tap sanemat/font
+
+echo "start brew install apps..."
+for formula in "${formulas[@]}"; do
+    brew install $formula || brew upgrade $formula
+done
+
+casks=(
+    dropbox
+    evernote
+    google-chrome
+    google-japanese-ime
+    slack
+    iterm2
+    clipy
+    visual-studio-code
+    docker
+)
+
+echo "start brew cask install apps..."
+for cask in "${casks[@]}"; do
+    brew cask install $cask
+done
+
+brew cleanup
+brew cask cleanup
+
+cat << END
+
+**************************************************
+HOMEBREW INSTALLED! bye.
+**************************************************
+
+END
