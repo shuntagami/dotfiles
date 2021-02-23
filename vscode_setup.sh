2664cf272f40dabfd80da958876669c75d89a99b
shuntagami <shuntagami23@gmail.com>
2021-02-23 20:43:13 +0900

    Refactoring(set current dirctory as variable)

--- vscode_setup.sh
+++ vscode_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+set -e
+DOT_DIRECTORY="${HOME}/dotfiles"
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-ln -sf "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
-
-ln -sf "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+ln -sf "${DOT_DIRECTORY}"/settings.json "${VSCODE_SETTING_DIR}"/settings.json
+ln -sf "${DOT_DIRECTORY}"/keybindings.json "${VSCODE_SETTING_DIR}"/keybindings.json
 
 cat < ./extensions | while read -r line
 do
