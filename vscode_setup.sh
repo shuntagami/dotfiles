9736b959231fa217f558fad9dbb76669927daa05
shuntagami <shuntagami23@gmail.com>
2021-02-23 20:01:13 +0900

    remove comment

--- vscode_setup.sh
+++ vscode_setup.sh
@@ -3,14 +3,8 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-# if [ -L "${VSCODE_SETTING_DIR}/settings.json" ]; then
-#   rm "${VSCODE_SETTING_DIR}/settings.json"
-# fi
 ln -sf "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-# if [ -L "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
-#   rm "${VSCODE_SETTING_DIR}/keybindings.json"
-# fi
 ln -sf "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
 cat < ./extensions | while read -r line
