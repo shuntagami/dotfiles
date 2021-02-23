c96fea3554f3ad92c2494e692709eba063bc697e
shuntagami <shuntagami23@gmail.com>
2021-02-23 17:12:17 +0900

    add vscode_sutup

---
+++ vscode_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+
+# if [ -L "${VSCODE_SETTING_DIR}/settings.json" ]; then
+#   rm "${VSCODE_SETTING_DIR}/settings.json"
+# fi
+ln -sf "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+
+# if [ -L "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
+#   rm "${VSCODE_SETTING_DIR}/keybindings.json"
+# fi
+ln -sf "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+
+cat < ./extensions | while read -r line
+do
+  code --install-extension "$line"
+done
+code --list-extensions > extensions
