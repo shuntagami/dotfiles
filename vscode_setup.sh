eff212f6840533ede2ece93a5062c7385d817f79
shuntagami <shun.tagami@i-nations.tokyo>
2021-07-15 16:11:12 +0900

    Add tsconfig.json

--- vscode_setup.sh
+++ vscode_setup.sh
@@ -6,6 +6,7 @@ VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
 ln -sf "${DOT_DIRECTORY}"/settings.json "${VSCODE_SETTING_DIR}"/settings.json
 ln -sf "${DOT_DIRECTORY}"/keybindings.json "${VSCODE_SETTING_DIR}"/keybindings.json
+ln -sf "${DOT_DIRECTORY}"/tsconfig.json "${VSCODE_SETTING_DIR}"/tsconfig.json
 
 cat < ./extensions | while read -r line
 do
