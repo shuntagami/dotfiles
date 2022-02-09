7c4caf5f3faebc479c9c3f93c17edf372c2146d4
shuntagami <shun.tagami@i-nations.tokyo>
2022-02-09 23:29:38 +0900

    fix

--- scripts/vscode_setup.sh
+++ scripts/vscode_setup.sh
@@ -8,7 +8,7 @@ ln -sf "${DOTFILES}"/misc/settings.json "${VSCODE_SETTING_DIR}"/settings.json
 ln -sf "${DOTFILES}"/misc/keybindings.json "${VSCODE_SETTING_DIR}"/keybindings.json
 ln -sf "${DOTFILES}"/misc/tsconfig.json "${VSCODE_SETTING_DIR}"/tsconfig.json
 
-cat < ./extensions | while read -r line
+cat < $DOTFILES/misc/extensions | while read -r line
 do
   code --install-extension "$line"
 done
