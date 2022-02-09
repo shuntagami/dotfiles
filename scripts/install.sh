1529e79ae8c42214983c0ebf5e4ccf4192e2fa8a
shuntagami <shun.tagami@i-nations.tokyo>
2022-02-10 01:08:37 +0900

    Add Brewfile

---
+++ scripts/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eux
+
+# Ask for the administrator password upfront
+sudo -v
+
+cd $HOME
+
+if ! type brew >/dev/null 2>&1; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! type git >/dev/null 2>&1; then
+  brew install git
+fi
+
+if [ ! -d dotfiles ]; then
+  git clone https://github.com/shuntagami/dotfiles.git && cd dotfiles && chmod +x ./scripts/*
+fi
+
+brew bundle install --file=$HOME/dotfiles/misc/.Brewfile
+
+./scripts/deploy.sh
