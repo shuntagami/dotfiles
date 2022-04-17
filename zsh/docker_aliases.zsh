9a583c54fe1e0e49e2527ee9d63681f66140362a
shuntagami <shun.tagami@i-nations.tokyo>
2022-04-17 23:50:43 +0900

    fix

--- zsh/docker_aliases.zsh
+++ zsh/docker_aliases.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
 # ------------------------------------
 # Docker alias and function
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
 function addToPath {
   case ":$PATH:" in
