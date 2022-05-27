5283de1bd5dfbd7be440c621815b684e68e25724
shuntagami <shun.tagami@i-nations.tokyo>
2022-05-27 23:55:36 +0900

    Update docker_aliases.zsh

--- zsh/docker_aliases.zsh
+++ zsh/docker_aliases.zsh
@@ -29,6 +29,10 @@ alias dki="docker run -i -t -P"
 # Execute interactive container, e.g., $dex base /bin/bash
 alias dex="docker exec -i -t"
 
+alias dc="docker compose"
+
+alias dcrm="docker compose run --rm"
+
 # Stop all containers
 dstop() { docker stop $(docker ps -a -q); }
 
@@ -41,11 +45,12 @@ alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 # Remove all images
 dri() { docker rmi $(docker images -q); }
 
-# Dockerfile build, e.g., $dbu tcnksm/test 
+# Dockerfile build, e.g., $dbu tcnksm/test
 dbu() { docker build -t=$1 .; }
+
+# Bash into running container
+dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
 
 # Show all alias related docker
 dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
 
-# Bash into running container
-dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
