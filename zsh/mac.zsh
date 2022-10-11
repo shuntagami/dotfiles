3b7ae7f598dc1d1b474ec747b3de8abcbd899fac
shuntagami <shuntagami23@gmail.com>
2022-10-11 23:36:08 +0900

    Update mac.zsh

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -27,10 +27,7 @@ alias chrome="open -a Google\ Chrome"
 # Recursively delete `.DS_Store` files
 alias rm-ds-store="find . -name '.DS_Store' -type f -delete"
 
-# Empty the Trash on all mounted volumes and the main HDD.
-# Also, clear Apple’s System Logs to improve shell startup speed.
-# Finally, clear download history from quarantine. https://mths.be/bum
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
+alias emptytrash="osascript -e 'tell application \"Finder\" to empty trash'"
 
 # Flush Directory Service cache
 alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
