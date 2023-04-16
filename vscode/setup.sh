32c41e508fde09eaf821d5d9f7613e99ebf71c38
shuntagami <shuntagami23@gmail.com>
2023-04-17 01:10:27 +0900

    update vscode/setup.sh

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -2,13 +2,25 @@
 
 set -eux
 
-[[ `uname` == "Darwin" ]] && VSCODE_SETTING_DIR=$HOME/Library/Application\ Support/Code/User
-[[ `uname` == "Linux" ]] && VSCODE_SETTING_DIR=$HOME/.config/Code/User
+if [[ `uname` == "Darwin" ]]; then
+  VSCODE_SETTING_DIR=$HOME/Library/Application\ Support/Code/User
+elif [[ `uname` == "Linux" ]] && [[ -n "${WSL_DISTRO_NAME}" ]]; then
+  VSCODE_SETTING_DIR=/mnt/c/Users/PC/AppData/Roaming/Code/User
+else
+  VSCODE_SETTING_DIR=$HOME/.config/Code/User
+fi
 
-ln -sf "${DOTFILES}"/vscode/settings.json "${VSCODE_SETTING_DIR}"/settings.json
-ln -sf "${DOTFILES}"/vscode/keybindings.json "${VSCODE_SETTING_DIR}"/keybindings.json
-ln -sf "${DOTFILES}"/vscode/tsconfig.json "${VSCODE_SETTING_DIR}"/tsconfig.json
-ln -sf "${DOTFILES}"/vscode/snippets "${VSCODE_SETTING_DIR}"/snippets
+if [[ `uname` == "Linux" ]] && [[ -n "${WSL_DISTRO_NAME}" ]]; then
+  cp -R "${DOTFILES}"/vscode/settings.json "${VSCODE_SETTING_DIR}"/settings.json
+  cp -R "${DOTFILES}"/vscode/keybindings.json "${VSCODE_SETTING_DIR}"/keybindings.json
+  cp -R "${DOTFILES}"/vscode/tsconfig.json "${VSCODE_SETTING_DIR}"/tsconfig.json
+  cp -R "${DOTFILES}"/vscode/snippets "${VSCODE_SETTING_DIR}"/snippets
+else
+  ln -sf "${DOTFILES}"/vscode/settings.json "${VSCODE_SETTING_DIR}"/settings.json
+  ln -sf "${DOTFILES}"/vscode/keybindings.json "${VSCODE_SETTING_DIR}"/keybindings.json
+  ln -sf "${DOTFILES}"/vscode/tsconfig.json "${VSCODE_SETTING_DIR}"/tsconfig.json
+  ln -sf "${DOTFILES}"/vscode/snippets "${VSCODE_SETTING_DIR}"/snippets
+fi
 
 cat < $DOTFILES/vscode/extensions | while read -r line
 do
