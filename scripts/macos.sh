0dded0e48f2bb7ea3427999bcc5868bce1b13bd4
shuntagami <shuntagami23@gmail.com>
2023-09-30 00:58:02 +0900

    tweak

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -353,9 +353,6 @@ defaults write com.apple.dock "orientation" -string "left"
 # Reset Launchpad, but keep the desktop wallpaper intact
 find "${HOME}/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
 
-# Add iOS & Watch Simulator to Launchpad
-sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app" "/Applications/Simulator.app"
-sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator (Watch).app" "/Applications/Simulator (Watch).app"
 
 # Hot corners
 # Possible values:
