10024963d95f1dc37e469fb53ab9a66a438ab2b6
shuntagami <shuntagami23@gmail.com>
2023-09-30 00:55:57 +0900

    Show only open applications in the Dock

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -306,10 +306,10 @@ defaults write com.apple.dock show-process-indicators -bool true
 # Wipe all (default) app icons from the Dock
 # This is only really useful when setting up a new Mac, or if you don’t use
 # the Dock to launch apps.
-#defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock persistent-apps -array
 
 # Show only open applications in the Dock
-#defaults write com.apple.dock static-only -bool true
+defaults write com.apple.dock static-only -bool true
 
 # Don’t animate opening applications from the Dock
 defaults write com.apple.dock launchanim -bool false
