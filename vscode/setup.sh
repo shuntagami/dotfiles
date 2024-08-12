54f809dd91f06b1d2a6a23a9cf503f9ed2520f69
shuntagami <shuntagami23@gmail.com>
2024-08-12 13:53:37 +0900

    update

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -5,7 +5,7 @@ set -eux
 if [[ `uname` == "Darwin" ]]; then
   VSCODE_SETTING_DIR=$HOME/Library/Application\ Support/Code/User
 elif [[ `uname` == "Linux" ]] && [[ -n "${WSL_DISTRO_NAME}" ]]; then
-  VSCODE_SETTING_DIR=/mnt/c/Users/PC/AppData/Roaming/Code/User
+  VSCODE_SETTING_DIR=/mnt/c/Users/shunt/AppData/Roaming/Code/User
 else
   VSCODE_SETTING_DIR=$HOME/.config/Code/User
 fi
@@ -15,17 +15,12 @@ if [[ `uname` == "Linux" ]] && [[ -n "${WSL_DISTRO_NAME}" ]]; then
   cp -R "${DOTFILES}"/vscode/keybindings.json "${VSCODE_SETTING_DIR}"/keybindings.json
   cp -R "${DOTFILES}"/vscode/tsconfig.json "${VSCODE_SETTING_DIR}"/tsconfig.json
   cp -R "${DOTFILES}"/vscode/snippets "${VSCODE_SETTING_DIR}"
+  for line in $(cat "$DOTFILES/vscode/extensions"); do
+    code --install-extension "$line" --force
+  done
 else
   ln -sf "${DOTFILES}"/vscode/settings.json "${VSCODE_SETTING_DIR}"/settings.json
   ln -sf "${DOTFILES}"/vscode/keybindings.json "${VSCODE_SETTING_DIR}"/keybindings.json
   ln -sf "${DOTFILES}"/vscode/tsconfig.json "${VSCODE_SETTING_DIR}"/tsconfig.json
   ln -sf "${DOTFILES}"/vscode/snippets "${VSCODE_SETTING_DIR}"
 fi
-
-# cat < $DOTFILES/vscode/extensions | while read -r line
-# do
-#   code --install-extension "$line" --force
-# done
-
-# cp /dev/null $DOTFILES/vscode/extensions
-# code --list-extensions > $DOTFILES/vscode/extensions
