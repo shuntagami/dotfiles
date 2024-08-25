31b2417f17668fc1db42f2b6923a30c057018a68
shuntagami <shuntagami23@gmail.com>
2024-08-25 09:01:59 +0900

    add path to dotnet tools

--- win/exports.ps1
+++ win/exports.ps1
@@ -2,5 +2,14 @@
 Set-Environment "EDITOR" "vim"
 Set-Environment "GIT_EDITOR" $Env:EDITOR
 
+# Add .dotnet/tools to the PATH environment variable
+$dotnetToolsPath = "$HOME\.dotnet\tools"
+if ($Env:Path -notlike "*$dotnetToolsPath*") {
+   $Env:Path = "$Env:Path;$dotnetToolsPath"
+}
+
+# Optionally, save the new PATH permanently for the user
+[System.Environment]::SetEnvironmentVariable("Path", $Env:Path, [System.EnvironmentVariableTarget]::User)
+
 # Disable the Progress Bar
 $ProgressPreference='SilentlyContinue'
