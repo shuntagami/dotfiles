9b20cb448b144e323b1ed0967e5d34cafbcc340b
shuntagami <shuntagami23@gmail.com>
2024-09-12 09:30:10 +0900

    update

--- zsh/wsl.zsh
+++ zsh/wsl.zsh
@@ -1,2 +1,9 @@
 # open is aliased to xdg-open in WSL
 alias xdg-open="explorer.exe"
+
+alias dls="cd /mnt/c/Users/shunt/Downloads"
+alias dt="cd /mnt/c/Users/shunt/Desktop"
+
+# winとwslでユーザー名揃えたら動く
+# alias dls='cd /mnt/c/Users/$(whoami)/Downloads'
+# alias dt='cd /mnt/c/Users/$(whoami)/Desktop'
