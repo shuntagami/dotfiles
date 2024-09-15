cd26b82a30c7524b85ff0006d0a66dfefd54db69
shuntagami <shuntagami23@gmail.com>
2024-09-15 21:15:48 +0900

    update

--- zsh/ubuntu.zsh
+++ zsh/ubuntu.zsh
@@ -5,3 +5,5 @@ alias d="cd ~/dotfiles"
 alias p="cd ~/projects"
 alias g="git"
 alias bat="batcat"
+
+alias cl1='xclip -selection clipboard -o | head -n 1 | while IFS= read -r line; do printf "%s" "$line"; done | xclip -selection clipboard'
