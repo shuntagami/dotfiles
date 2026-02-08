98ea96ed03a587f6d09f8d1e918fa4816aa8681d
shuntagami <shuntagami23@gmail.com>
2026-02-09 01:04:58 +0900

    h1downでh1は残す

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -207,7 +207,7 @@ update-brew-env() {
 alias update-from-brewfile='update-brew-env from-brewfile'
 alias update-from-system='update-brew-env from-system'
 
-alias h1down='sed -i "" -E -e "s/^# (.*)$/\\1/" -e "s/^#(#+) /\\1 /"'
+alias h1down='sed -i "" -E "s/^#(#+) /\\1 /"'
 alias h1up='sed -i "" -E "s/^(#+ .*)/#\\1/"'
 alias nobold='sed -i "" -E '\''s/\*\*([^*]+)\*\*/\1/g'\'''
 noboldh() {
